02bebe6d99452059246fa2d0ce0a985f18cd4830
ksera524 <ksera631@gmail.com>
2025-07-21 21:21:48 +0900

    制限緩和

--- src/routes/slack.rs
+++ src/routes/slack.rs
@@ -6,7 +6,7 @@ use axum::{extract::DefaultBodyLimit, routing::post, Router};
 
 pub fn create_slack_routes() -> Router<AppState> {
     Router::new()
-        .layer(DefaultBodyLimit::disable())
+        .layer(DefaultBodyLimit::max(100 * 1024 * 1024)) // 100MB制限
         .route("/slack/message", post(post_message))
         .route("/slack/upload_base64", post(upload_file_base64))
 }
